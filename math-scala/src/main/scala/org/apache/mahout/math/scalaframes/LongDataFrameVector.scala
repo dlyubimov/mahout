a9262043bfa004d2d0258edd34e255ab18341c7b
Saikat Kanjilal <sxk1969@hotmail.com>
2014-05-30 21:34:22 -0700

    iterator access for String,Long and Integer and Double with a set of tests to go with it

--- math-scala/src/main/scala/org/apache/mahout/math/scalaframes/LongDataFrameVector.scala
+++ math-scala/src/main/scala/org/apache/mahout/math/scalaframes/LongDataFrameVector.scala
@@ -4,7 +4,23 @@ import scala.concurrent.util.Unsafe
 import scala.Long
 
 
-abstract class LongDataFrameVector extends DataFrameVectorLike[Long] {
+class LongDataFrameVector(vectorOfLongs:Vector[Long]) extends DataFrameIteratorLike[Long] {
 	var data:Unsafe=_
+	var curPosition:Int=0
+	def apply(x:Int): Long = {
+	  return vectorOfLongs.apply(x)
+	}
 	
+	//stub these out for now
+	def hasNext: Boolean = {
+	  return true;
+	}
+	
+	//stub this to return the current position
+	//will need to figure meaning of current position
+	def next: Long = {
+	  return vectorOfLongs.apply(curPosition);
+	}
+   
+		
 }
